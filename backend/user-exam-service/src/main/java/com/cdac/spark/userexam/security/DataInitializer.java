483bae50854e1efd303bef41848cc94a1fa93e2b
Prachi <prachi.rai.cmfeb26@gmail.com>
2026-08-07 01:46:26 +0530

    Add Question client service

--- backend/user-exam-service/src/main/java/com/cdac/spark/userexam/security/DataInitializer.java
+++ backend/user-exam-service/src/main/java/com/cdac/spark/userexam/security/DataInitializer.java
@@ -1,7 +1,28 @@
 package com.cdac.spark.userexam.security;
 
+import com.cdac.spark.userexam.entity.User;
+import com.cdac.spark.userexam.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 
+@Component
+public class DataInitializer implements CommandLineRunner {
 
-public class DataInitializer {
+    @Autowired
+    private UserRepository userRepository;
     
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... args) throws Exception {
+        User admin = userRepository.findByEmail("Admin@gmail.com").orElse(new User());
+        admin.setEmail("Admin@gmail.com");
+        admin.setFullName("Default Admin");
+        admin.setPassword(passwordEncoder.encode("Admin123"));
+        admin.setRole("ROLE_ADMIN");
+        userRepository.save(admin);
+    }
 }
