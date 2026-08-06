1ec89c4b2780250f0617f7ccd57b144d0e215652
Prachi <prachi.rai.cmfeb26@gmail.com>
2026-08-07 02:14:15 +0530

    add ForgotPasswordRequest and ResetPasswordRequest

--- backend/user-exam-service/src/main/java/com/cdac/spark/userexam/dto/req/ForgotPasswordRequest.java
+++ backend/user-exam-service/src/main/java/com/cdac/spark/userexam/dto/req/ForgotPasswordRequest.java
@@ -1,6 +1,15 @@
-package com.cdac.spark.userexam.dto.req;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ForgotPasswordRequest {
-    
+    @NotBlank(message = "Username/PRN is required")
+    private String username;
 }
